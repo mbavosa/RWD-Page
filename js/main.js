64de9a4f1c5728a0a8398faf6e5e4e798735a4a1
Matthew Bavosa <matthew.bavosa@gmail.com>
2017-03-30 18:04:59 -0400

    new js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ var nav = document.querySelector('nav');
 var links = document.getElementsByTagName('a');
 
 nav.addEventListener('click', function () {
-  console.log('Yes, it clicked!')
+  console.log('Yes, it clicked!');
+  links[1].classList.add('active')
 });
